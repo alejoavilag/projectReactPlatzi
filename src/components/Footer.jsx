c174fc1742d5f1ff31d6f2354eeea3073dbff4c1
Alejo Avila <alejandroavilaguerrero@gmail.com>
2020-05-04 19:31:18 -0500

    [ADD] project finish React

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../assets/styles/components/Footer.scss';
+
+const Footer = () => (
+  <footer className="footer">
+    <a href="/">Terminós de uso</a>
+    <a href="/">Declaraciones de privacidad</a>
+    <a href="/">Centro de ayuda</a>
+  </footer>
+);
+
+export default Footer;
